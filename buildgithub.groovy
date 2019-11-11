7d3d86ad295de57dde0b1c671f311116366f8beb
Zhu Xiang <zhu.xiang@partner.bmw.com>
2019-11-11 17:50:26 +0800

    d

--- buildgithub.groovy
+++ buildgithub.groovy
@@ -1,5 +1,45 @@
 package utilities
-import utilities.GithubMultibranch
+
+import javaposse.jobdsl.dsl.DslFactory
+
+class GithubMultibranch {
+ 
+    String name
+    String description
+    String displayName
+    String repositoryOwner
+    String repositoryName
+    String credentials
+    String includeBranches
+    String excludeBranches
+ 
+ 
+    void build(DslFactory dslFactory) {
+        def job = dslFactory.multibranchPipelineJob(name) {
+            description(description)
+            displayName(displayName)
+            branchSources {
+                github {
+                    scanCredentialsId(credentials)
+                    repoOwner(repositoryOwner)
+                    repository(repositoryName)
+                    includes(includeBranches)
+                    excludes(excludeBranches)
+                }
+            }
+            factory {
+                workflowBranchProjectFactory {
+                    scriptPath('.jenkins/Jenkinsfile')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(15)
+                }
+            }
+        }
+    }
+}
 
 def multiPipeline = new GithubMultibranch(
     description: 'Just try make world better',
