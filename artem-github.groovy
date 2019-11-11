ac2f867b9e95075377417e80f3dff34241a9ea35
Zhu Xiang <zhu.xiang@partner.bmw.com>
2019-11-11 16:49:01 +0800

    add multibranch job

---
+++ artem-github.groovy
@@ -0,0 +1,13 @@
+import utilities.GithubMultibranch
+ 
+ 
+def multiPipeline = new GithubMultibranch(
+    description: 'Just try make world better',
+    name: 'Github-Test',
+    displayName: 'Github-Test',
+    repositoryOwner: "yuezhuxiang",
+    repositoryName: "jenkinsci-example",
+    credentials: '0f57727d-e2bf-42ef-bdc5-94a53d7248c1',
+    includeBranches: 'development staging master branchA',
+    excludeBranches: ''
+).build(this)
