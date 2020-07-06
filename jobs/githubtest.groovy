954742b5aa8fe1e0c98e2431458c5f033ebae061
Zhu Xiang <zhu.xiang@partner.bmw.com>
2020-07-06 16:25:26 +0800

    add jenkins-example pipeline

--- jobs/githubtest.groovy
+++ jobs/githubtest.groovy
@@ -1,12 +1,8 @@
 import utils.*
 
 new BtcGithubPipeline()
-    .description('Just try make world better')
-    .name('Github-Test')
-    .displayName('Github-Test')
-    .repositoryOwner("yuezhuxiang")
-    .repositoryName("jenkinsci-example")
-    .credentials('0f57727d-e2bf-42ef-bdc5-94a53d7248c1')
-    .includeBranches('development master branchA')
-    .excludeBranches('')
+    .viewPrefix("core-services")
+    .projectKey("core-services")
+    .repository("jenkinsci-example")
+    .includes("*")
     .build(this)
