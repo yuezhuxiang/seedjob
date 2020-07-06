c6c6321214d73007603c1fa8eb6bf853e77b51e0
Zhu Xiang <zhu.xiang@partner.bmw.com>
2020-07-06 16:14:25 +0800

    add githubtest job

---
+++ jobs/github-test.groovy
@@ -0,0 +1,12 @@
+import utils.*
+
+new BtcGithubPipeline()
+    .description('Just try make world better')
+    .name('Github-Test')
+    .displayName('Github-Test')
+    .repositoryOwner("yuezhuxiang")
+    .repositoryName("jenkinsci-example")
+    .credentials('0f57727d-e2bf-42ef-bdc5-94a53d7248c1')
+    .includeBranches('development master branchA')
+    .excludeBranches('')
+    .build(
